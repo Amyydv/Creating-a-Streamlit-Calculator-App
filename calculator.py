7e485296e8d6f6f9eaaa9e8961203857f402cef9
Amit Singh Yadav <112614485+Amyydv@users.noreply.github.com>
2023-06-02 12:58:57 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,13 +1,37 @@
-
 import streamlit as st
-from app_pages.multi_page import MultiPage
 
-from app_pages.page_calculator import calculator_body
+st.title("Calculator App using Streamlit")
 
-app = MultiPage(app_name="Calculator App")  # Create an instance of the app
+# creates a horizontal line
+st.write("---")
 
+# input 1
+num1 = st.number_input(label="Enter first number")
 
-# Add your app pages here using .add_page()
-app.app_page("Calculator", calculator_body)
+# input 2
+num2 = st.number_input(label="Enter second number")
 
-app.run()         
+st.write("Operation")
+
+operation = st.radio("Select an operation to perform:",
+                    ("Add", "Subtract", "Multiply", "Divide"))
+
+ans = 0
+
+def calculate():
+    if operation == "Add":
+        ans = num1 + num2
+    elif operation == "Subtract":
+        ans = num1 - num2
+    elif operation == "Multiply":
+        ans = num1 * num2
+    elif operation=="Divide" and num2!=0:
+        ans = num1 / num2
+    else:
+        st.warning("Division by 0 error. Please enter a non-zero number.")
+        ans = "Not defined"
+
+    st.success(f"Answer = {ans}")
+
+if st.button("Calculate result"):
+    calculate()
