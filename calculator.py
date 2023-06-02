2fddc95d4e28549999b77a5d40dfcd6a09575e99
Amit Singh Yadav <112614485+Amyydv@users.noreply.github.com>
2023-06-02 12:56:01 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,33 +1,13 @@
+
 import streamlit as st
-st.title('Amy Simple Calculator')
-st.write('Simple calculator app')
+from app_pages.multi_page import MultiPage
 
-a = st.number_input('Enter a number')
-b = st.number_input('Enter another number')
-operation = st.selectbox('Select Operation', ['Add', 'Subtract', 'Multiply', 'Divide'])
+from app_pages.page_calculator import calculator_body
 
-def add(a, b):
-  return a + b
-def sub(a, b):
-  return a - b
-def mul(a, b):
-  return a * b
-def div(a, b):
-  return a / b
-def main():
-    st.title("Simple Calculator")
-st.write("This is a simple calculator app")
-a = st.number_input("Enter a number")
-b = st.number_input("Enter another number")
-operation = st.selectbox("Select Operation", ["Add", "Subtract", "Multiply", "Divide"])
-if operation == "Add":
-    st.write(add(a, b))
-elif operation == "Subtract":
-    st.write(sub(a, b))
-elif operation == "Multiply":
-    st.write(mul(a, b))
-elif operation == "Divide":
-    st.write(div(a, b))
-if __name__ == '__main__':
-     main()
-         
+app = MultiPage(app_name="Calculator App")  # Create an instance of the app
+
+
+# Add your app pages here using .add_page()
+app.app_page("Calculator", calculator_body)
+
+app.run()         
